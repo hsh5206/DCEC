227cc73084c039461147428eb5015c981d1a1f6b
hsh5206 <about5206@naver.com>
2021-05-21 15:46:48 +0900

    screens/Signu styles use Platformp

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,13 +1,44 @@
 import React from 'react'
 import {
+  StyleSheet,
   SafeAreaView,
+  View,
   Text
 } from 'react-native'
 
 export default function Login() {
   return(
     <SafeAreaView style={{flex:1, backgroundColor:'white'}}>
-      <Text>Home</Text>
+      <View style={styles.header}><Text>header</Text></View>
+      <View style={styles.content}><Text>content</Text></View>
+      <View style={styles.footer}><Text>footer</Text></View>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  header: {
+    height: '20%',
+    marginHorizontal:'5%',
+    backgroundColor: 'green',
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  content: {
+    flex:1,
+    marginHorizontal:'5%',
+    backgroundColor: 'red',
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  footer: {
+    height: '20%',
+    marginHorizontal:'5%',
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'center'
+  }
+})
--- src/screens/Signup.tsx
+++ src/screens/Signup.tsx
@@ -5,7 +5,8 @@ import {
   View,
   Text,
   TextInput,
-  TouchableOpacity
+  TouchableOpacity,
+  Platform
 } from 'react-native'
 import {Colors} from 'react-native-paper'
 
@@ -15,33 +16,39 @@ const onPress = () => {
 
 export default function Login() {
   return(
-    <SafeAreaView style={{flex:1, backgroundColor:'white'}}>
-      <View style={styles.header}>
+    <SafeAreaView style={{flex:1}}>
+      <View style={[styles.header,{marginTop:'25%'}]}>
         <View style ={{flex: 0.8, marginTop: '3%'}}>
-        <Text style={{fontSize: 25, marginLeft: '3%', marginTop:'8%'}}>회원가입</Text>
+        <Text style={{fontSize: 25, marginLeft: '3%', marginTop: Platform.select({ios : '10%', android: '6%'})}}>회원가입</Text>
         </View>
         <View style={{width: '95%', borderBottomWidth:1, borderColor:'#444'}}/>
       </View>
 
-      <View style={styles.content}>
+      <View style={[styles.content]}>
         <View style={[styles.object]}>
           <TextInput placeholder="아이디 입력" style={styles.inputText}/>
-          <TouchableOpacity style={styles.loginButton}>
+          <TouchableOpacity style={styles.checkButton}>
             <Text style={{color:'white', fontSize:18, fontWeight:'300'}} onPress={onPress}>중복확인</Text>
           </TouchableOpacity>
         </View>
         <View style={[styles.object]}>
-          <TextInput placeholder="비밀번호 입력 (영문+숫자+특수문자 8~15자)" style={[styles.inputText, {width:'100%',}]}/>
+          <TextInput placeholder="비밀번호 입력 (영문+숫자+특수문자 8~15자)" style={[styles.inputText, {width:'100%'}]}/>
         </View>
         <View style={[styles.object]}>
-          <TextInput placeholder="비밀번호 확인"style={[styles.inputText, {width:'100%',}]}/>
+          <TextInput placeholder="비밀번호 확인"style={[styles.inputText, {width:'100%'}]}/>
+        </View>
+        <View style={[styles.object]}>
+          <TextInput placeholder="이름"style={[styles.inputText, {width:'100%'}]}/>
+        </View>
+        <View style= {[{alignItems:'center'}]}>
+          <TouchableOpacity style={styles.signupButton}>
+            <Text style={{color:'white', fontSize:18, fontWeight:'300'}} onPress={onPress}>회원가입</Text>
+          </TouchableOpacity>
         </View>
       </View>
       
       <View style={styles.footer}>
-      <TouchableOpacity style={styles.signupButton}>
-            <Text style={{color:'white', fontSize:18, fontWeight:'300'}} onPress={onPress}>회원가입</Text>
-          </TouchableOpacity>
+        
       </View>
     </SafeAreaView>
   )
@@ -58,7 +65,7 @@ const styles = StyleSheet.create({
     marginHorizontal:'10%',
   },
   footer: {
-    height: '20%',
+    height: '15%',
     marginHorizontal:'5%',
     alignItems: 'center',
     flexDirection: 'row',
@@ -66,20 +73,20 @@ const styles = StyleSheet.create({
   },
   inputText: {
     width:'70%',
-    height:'85%',
+    height:'90%',
     marginVertical: '1%',
     borderWidth: 2,
     borderColor: Colors.grey500,
     borderRadius: 10
   },
   object: {
-    flex:0.1,
+    height:50,
     flexDirection:'row',
     justifyContent:'space-between',
     alignItems:'center',
     paddingBottom:10
   },
-  loginButton: {
+  checkButton: {
     width: '29%',
     height: '85%',
     justifyContent: 'center',
@@ -88,10 +95,11 @@ const styles = StyleSheet.create({
     borderRadius: 10
   },
   signupButton: {
-    width: '50%',
-    height: '40%',
+    width: '90%',
+    height: '35%',
     justifyContent: 'center',
     alignItems: 'center',
+    marginVertical: '1%',
     backgroundColor: Colors.grey800,
     borderRadius: 10
   }
